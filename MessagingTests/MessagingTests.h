f284cd091078aaa7bbf6fb0be6177e195f51e354
Scott Nicol <s.cott@me.com>
2013-01-11 15:58:06 +0000

    Initial Commit

---
+++ MessagingTests/MessagingTests.h
@@ -0,0 +1,13 @@
+//
+//  MessagingTests.h
+//  MessagingTests
+//
+//  Created by Scott Nicol on 11/01/2013.
+//  Copyright (c) 2013 Scott Nicol. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MessagingTests : SenTestCase
+
+@end
---
+++ MessagingTests/MessagingTests.m
@@ -0,0 +1,32 @@
+//
+//  MessagingTests.m
+//  MessagingTests
+//
+//  Created by Scott Nicol on 11/01/2013.
+//  Copyright (c) 2013 Scott Nicol. All rights reserved.
+//
+
+#import "MessagingTests.h"
+
+@implementation MessagingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MessagingTests");
+}
+
+@end
